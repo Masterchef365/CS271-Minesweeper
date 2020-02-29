fe349819f8664f51f63956f0ad1a009d0a7c4bd2
Masterchef365 <duncan.freeman1@gmail.com>
2020-02-28 16:39:06 -0800

    Change ssembly makefile to include Irvine port

--- assembly_prototyping/minesweeper.s
+++ assembly_prototyping/minesweeper.s
@@ -1,52 +1,25 @@
+%include 'Along32.inc'
+
 ; Note that this is NOT MASM code, nor does it use the 
-; Irvine libraries. It's just a test bed for concepts.
+; Ievine libraries. It's just a test bed for concepts.
 
 extern exit
-extern printf
-extern scanf
-extern puts
-extern putchar
-
-; C x64 calling convention, in order:
-; rdi, rsi, rdx, rcx, r8, r9
-; return rax
 
 global main
 
 section .data
     greeting db "Greetings, Duncan", 0x00 
-    format_int db "%i", 0
+    greeting_len equ $-greeting
 
 section .bss
-    outbuf resd 1
 
 section .text
     
 main:
     ; Align the stack pointer before C calls. THIS IS NECESSARY.
-    sub rsp, 8
-
-    ; Print a greeting
-    mov edi, greeting
-    call puts
-
-    ; Read an integer
-    mov rdi, format_int
-    mov rsi, outbuf
-    mov al, 0
-    call scanf
-
-    ; Write an integer
-    mov rdi, format_int
-    mov rsi, [outbuf]
-    call printf
-
-    ; Write a newline
-    mov rdi, 0x0A
-    call putchar
-
-    ; Pop the garbage stack alloc used for alignment
-    add rsp, 8
+    mov eax, 90
+    call WriteInt
+    call Crlf
 
 stop:
     ; Exit with EXIT_SUCCESS
